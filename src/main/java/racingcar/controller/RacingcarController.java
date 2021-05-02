17f9a245464595e0b3c1d40aab9db5c5c6dace57
shkwon <oper912@naver.com>
2021-05-03 02:03:03 +0900

    feat: 게임 진행 담당 클래스 추가 및 자동차 이동 구현

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,16 +1,28 @@
 package racingcar.controller;
 
+import racingcar.domain.RacingGame;
+import racingcar.strategy.MoveableStrategy;
+import racingcar.strategy.RandomMoveableStrategy;
 import racingcar.view.InputView;
+
+import java.util.List;
 
 public class RacingcarController {
     private final InputView inputView;
+    private final MoveableStrategy moveableStrategy;
 
     public RacingcarController() {
         this.inputView = new InputView();
+        this.moveableStrategy = new RandomMoveableStrategy();
     }
 
     public void run() {
-        inputView.inputCarNames();
-        inputView.inputMoveCount();
+        List<String> carNames = inputView.inputCarNames();
+        int moveCount = inputView.inputMoveCount();
+        RacingGame racingGame = RacingGame.init(carNames, moveCount);
+
+        while (racingGame.isRaceable()) {
+            racingGame.race(moveableStrategy);
+        }
     }
 }
---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import racingcar.strategy.MoveableStrategy;
+
+import java.util.List;
+
+public class RacingGame {
+    private final Cars cars;
+    private final MoveCount moveCount;
+
+    private RacingGame(final Cars cars, final MoveCount moveCount) {
+        this.cars = cars;
+        this.moveCount = moveCount;
+    }
+
+    public static RacingGame init(final List<String> carNames, final int moveCount) {
+        return new RacingGame(Cars.from(carNames), MoveCount.from(moveCount));
+    }
+
+    public boolean isRaceable() {
+        return moveCount.isMoveable();
+    }
+
+    public void race(MoveableStrategy moveableStrategy) {
+        cars.move(moveableStrategy);
+        moveCount.minus();
+    }
+}
